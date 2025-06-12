ca343a7bfeeb574af4d0d061ce05ddef1557c89f
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-06-12 12:00:45 +0200

    feat

--- C/Prove/6Sett23/main.c
+++ C/Prove/6Sett23/main.c
@@ -1,15 +1,9 @@
 #include <stdio.h> //Standard I/O: printf, BUFSIZ
 #include <fcntl.h> //File CoNTrol: open, O_RDONLY, O_WRONLY, O_RDWR, O_CREAT(crea se non esiste), O_APPEND(solo R o RW)
-//O_TRUNC(distrugge contenuto se esiste, O_EXCL(fallisce se il file esiste gia'), O_NDELAY (non bloccante per FIFO e pipe in W)
 #include <stdlib.h> //STanDard LIBrary: exit, malloc, calloc, atoi
 #include <unistd.h> //UNIx Standard: read, write, close, SEEK_SET, SEEK_CUR, SEEK_END
-#include <string.h> //STRING: strlen, strcpy, strcat
-#include <sys/stat.h> //SYStem STATus: stat, fstat, S_IFMT, S_IFDIR, S_IFREG
 #include <sys/wait.h> //SYStem WAIT: wait
-#include <errno.h> //ERror NUmber: errno
-#include <sys/types.h> //SYStem TYPES: pid_t
-#include <ctype.h> //ISoType: isdigit, isalpha, isalnum
-#define PERM 077 /*Permessi da dare al file creato*/
+
 typedef int pipe_t[2]; /* Tipo per creare più pipe */
 
 typedef struct 
