c83fd234e58b29bb29544f29a158b15d65c5ce55
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-02 17:27:06 +0200

    Prova-8Set21

--- C/Prove/8Set21/main.c
+++ C/Prove/8Set21/main.c
@@ -12,20 +12,21 @@
 #define PERM 777 /*Permessi da dare al file creato*/
 typedef int pipe_t[2]; /* Tipo per creare più pipe */
 
+
 int main(int argc, char* argv[]) {
     
 
+
     /*------ VARIABILI USATE ------*/
-    int fd,fcreato;                    /* file descriptor */
-    char Cx,linea[250];                   /* variabile che conterrà un singolo carattere */
+    int fd;                    /* file descriptor */
+    char Cx;                   /* variabile che conterrà un singolo carattere */
     int pid;                   /* Variabile che conterrà il valore di ritorno di fork */
     int status;                /* Variabile che conterrà lo stato di un figlio */
     int pidFiglio;             /* Variabile che conterrà il pid del figlio*/
     int ritorno;               /* Variabile che conterrà il ritrono del filgio*/
     int N;                     /* Numero di figli */
-    int n,riga,i,n1,n2;                     /* Contatore */
-    pipe_t* dispari;   /* Canale di comunicazione padre-filgio */
-    pipe_t* pari;   /* Canale di comunicazione padre-filgio */
+    int n,linee,i,j,k,L1,L2;                     /* Contatore */
+    pipe_t* p;                 /* Canale di comunicazione padre-filgio */
 
 
     /*Controllo parametri LASCO*/
@@ -33,49 +34,28 @@ int main(int argc, char* argv[]) {
         printf("ERRORE: numero parametri insufficiente\nSUGGERIMENTO: inserire almeno 2 argomenti\n");
         exit(-1);
     }
-
-
     N=argc-1;
 
-
-    /*Creazione del file */
-    if ((fcreato=creat("dicandia.log", PERM)) < 0){
-       printf("DEBUG-FILGIO Impossibile creare il file %s\n", "DICANDIA");
-       exit(-1);
-    }
-
-
     /* allocazione dell'array di N pipe */
-    pari = (pipe_t *)malloc(N * sizeof(pipe_t));
-    if (pari == NULL) {
+    p = (pipe_t *)malloc(2*N * sizeof(pipe_t));
+    if (p == NULL) {
        printf("Errore nella allocazione array dinamico per le pipe\n");
        exit(3);
     }
     /* Creazione delle N pipe */
     for(n=0;n<N;n++){
-       if(pipe(pari[n]) < 0 ){
-          printf("ERRORE: qualcosa e' andato storto nella creazione delle pipe\n");
-          exit(3);
-       }
-    }
-    
-    
-    /* allocazione dell'array di N pipe */
-    dispari = (pipe_t *)malloc(N * sizeof(pipe_t));
-    if (dispari == NULL) {
-       printf("Errore nella allocazione array dinamico per le pipe\n");
-       exit(3);
-    }
-    /* Creazione delle N pipe */
-    for(n=0;n<N;n++){
-       if(pipe(dispari[n]) < 0 ){
+       if(pipe(p[n]) < 0 ){
           printf("ERRORE: qualcosa e' andato storto nella creazione delle pipe\n");
           exit(3);
        }
     }
 
-
-
+    // /*Creazione del file */
+    // if ((fw=creat("dicandia.log", PERM)) < 0){
+    //    printf("DEBUG-FILGIO Impossibile creare il file %s\n", "dicandia.log" );
+    //    exit(-1);
+    // }
+    // /* chiudiamo il file creato dato che comunque il figlio non lo usa */
 
     for(n=0;n<N*2;n++){
 
@@ -87,123 +67,127 @@ int main(int argc, char* argv[]) {
         }
         if (pid == 0) {
          /* codice eseguito dal processo figlio*/
+           
 
 
-            if(n%2){ /* Se pari*/
-
-                /* Chiusura del canale di lettura di N pipe */
-                for(int k=0;k<N;k++){
-                   close(pari[k][0]);
-                   if(k != n){
-                        close(pari[k][1]); /* Chiusura dei canali di scrittura che non ci interessano */
-                   }
-                }
-
-                close(dispari[n][0]);
-                close(dispari[n][1]);
-
-            }else { /* Se dispari*/
-
-
-                /* Chiusura del canale di lettura di N pipe */
-                for(int k=0;k<N;k++){
-                   close(dispari[k][0]);
-                   if(k != n){
-                        close(dispari[k][1]); /* Chiusura dei canali di scrittura che non ci interessano */
-                   }
-                }
-
-                close(pari[n][1]);
-                close(pari[n][0]);
-
-
+            /* Chiusura del canale di lettura di N pipe */
+            for(int k=0;k<N;k++){
+               close(p[k][0]);
+               if(k != n){
+                    close(p[k][1]); /* Chiusura dei canali di scrittura che non ci interessano */
+               }
             }
 
 
+            if(n%2 == 0){ /* FIGLI PARI */
 
-            /* controllo che il parametro sia un file */
-            if((fd=open(argv[n/2+1],O_RDONLY)) < 0 ){ //APRO IL FILE
-               printf("ERRORE: parametro %s non valido\nSUGGERIMENTO: inserire il nome di un file\n",argv[n/2+1]);
-               exit(-1);
-            }
+                /* controllo che il parametro sia un file */
+                if((fd=open(argv[n/2 + 1],O_RDONLY)) < 0 ){ //APRO IL FILE
+                   printf("ERRORE: parametro %s non valido\nSUGGERIMENTO: inserire il nome di un file\n",argv[n/2 + 1]);
+                   exit(-1);
+                }
 
-            riga=0;
-            i=0;
-            ritorno=-1;
-            while((read(fd,&Cx,1)) > 0){ /* Leggo il contenuto del file */
-               
-                if(Cx=='\n'){
-                    i++;
-                    printf("n:%d riga:%d\n",n,riga);
-                    if(((n%2) == 0) && ((riga%2) == 0)){ /* Pari */
-                        printf("DEBUG-FIGLIO %d scrivo sulla pipe la riga %d\n",n,riga);
-                        if(write(pari[n/2][1],&i,sizeof(i)) < 0){ /* Leggo il contenuto del file */
-                           printf("Errore nella scrittura 139 %d\n",n);
-                           exit(-1);
+
+                i=0;
+                linee=0;
+                ritorno = 0;
+                while (read(fd,&Cx,1) == 1){
+
+                    if(Cx == '\n'){
+                        linee++;
+                        if(linee%2 == 0){
+                            if((write(p[n][1],&i,sizeof(i))) < 0){ /* Scrivo */
+                               printf("ERRORE: qualcosa è andato storto nella scrittura \n");
+                               exit(-1);
+                            }
+                            if(i > ritorno){
+                                ritorno = i;
+                            }
                         }
 
-                        if(ritorno < i){
-                            ritorno = i;
-                        }
+                        i=0;
+                    }else i++;
 
-                    }else if(((n%2) != 0) && ((riga%2) != 0)){ /* Dispari */
-                        printf("DEBUG-FIGLIO %d scrivo sulla pipe la riga %d\n",n,riga);
-
-                        if((write(dispari[n/2][1],&i,sizeof(i))) < 0){ /* Leggo il contenuto del file */
-                           printf("Errore nella scrittura 146\n");
-                           exit(-1);
-                        }
-                    
-
-                        if(ritorno < i){
-                            ritorno = i;
-                        }
-
-                    }
-                    riga++;
-                    i=0;
-                }else i++;
+                }
                 
 
+            }else { /* FIGLI DISPARI */
 
-            }           
-           exit(ritorno);
+                /* controllo che il parametro sia un file */
+                if((fd=open(argv[(n+1)/2],O_RDONLY)) < 0 ){ //APRO IL FILE
+                   printf("ERRORE: parametro %s non valido\nSUGGERIMENTO: inserire il nome di un file\n",argv[(n+1)/2]);
+                   exit(-1);
+                }
+
+
+
+
+                i=0;
+                linee=0;
+                ritorno = 0;
+                while (read(fd,&Cx,1) == 1){
+
+                    if(Cx == '\n'){
+                        linee++;
+                        if((linee%2) != 0){
+                            if((write(p[n][1],&i,sizeof(i))) < 0){ /* Scrivo */
+                               printf("ERRORE: qualcosa è andato storto nella scrittura \n");
+                               exit(-1);
+                            }
+                            if(i > ritorno){
+                                ritorno = i;
+                            }
+                        }
+
+                        i=0;
+                    }else i++;
+
+                }
+               
+
+            }
+            exit(ritorno);
+
+
         }
-
+        
 
     }
 
 
     /* Chiusura del canale di scrittura di N pipe */
-    for(n=0;n<N;n++){
-       close(pari[n][1]);
-       close(dispari[n][1]);
-    }
+  for (n=0; n < 2*N; n++){
+		close(p[n][1]);
+   }
+   
+   for(i=0;i<N;i++){
 
+        //printf("LUNGHEZZE FILE %s\n",argv[i + 1]);
+        
+        linee=1;
+        do{
+            /* il padre recupera le lunghezze delle linee da ogni figlio dispari e pari */
+	       	k=read(p[i*2+1][0], &L1, sizeof(L1));
+			j=read(p[i*2][0], &L2, sizeof(L2));
+        
+            if(k != 0){
+                printf("    Lunghezza della linea %d: %d k:%d\n",linee,L1,k);
+                linee++;
+            }
+        
+            if(j != 0){
+                printf("    Lunghezza della linea %d: %d j:%d\n",linee,L2,j);
+                linee++;
+            }
 
-    for(n=0; n < N; n++){
+        }while (j||k);
       
-        printf("Lunghezza righe del file %d\n",1);
-        i=0;
-        while (read(dispari[n][1],&n1,sizeof(int)) || read(pari[n][1],&n2,sizeof(int)))
-        {
-            i++;
-            
-            if(n1){
-                printf("la lunghezza della riga %d è %d\n",i,n1);
-            }
-            if(n2){
-                printf("la lunghezza della riga %d è %d\n",i,n2);
-            }
-
-        }
+        
 
     }
 
 
-
-    for(n=0;n<N*2;n++){
-
+    for(n=0; n<N*2; n++){
         if((pidFiglio = wait(&status)) < 0){ // assegamo a pid il pid del figlio e a status lo stato del filgio 
             printf("ERRORE: wait ha fallito\n");
             exit(-1);
@@ -215,10 +199,8 @@ int main(int argc, char* argv[]) {
             ritorno = status >> 8;
             /* selezione degli 8 bit piu’ significativi */
             ritorno &= 0xFF;
-            sprintf(linea,"Il filgio %d con pid %d ha ritornato %d\n",n,pidFiglio,ritorno);
-            write(fcreato,linea,sizeof(linea));
+            printf("Il filgio %d con pid %d ha ritornato %d\n",n,pidFiglio,ritorno);
         }
-
     }
 
     exit(0);
