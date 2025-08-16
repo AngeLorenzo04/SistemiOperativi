1a6e84413ec7e6f6c2223df1fa263289d16963dc
AngeLoreno04 <dicandiangelorenzo@gmail.com>
2025-08-16 19:06:03 +0200

    Shell Esame12Apr23

--- Shell/Esame12Apr23/FCP.sh
+++ Shell/Esame12Apr23/FCP.sh
@@ -2,81 +2,102 @@
 #FIle comandi principale inerente alla prova d'esame del 12 Aprile 2023
 
 
-#Controllo parametri LASCO
+#Controllo comandi LASCO
 if test $# -lt 3
 then
-    echo ERRORE: numero parametri insufficiente
-    echo SUGGERIMENTO: Inserire alemno 4 parametri
-    exit 1
+   echo ERRORE: numero parametri non valido
+   echo SUGGERIMENTO: inserire almeno 4 parametri
+   exit 1
 fi
 
-#Salvo la prima stringa in una variabile
+#Salvo le prime due stringhe
 S1=$1
 shift
-#Salvo la seconda stringa in una variabile
 S2=$1
 shift
 
-#Controllo che gli altri parametri siano validi
+#Controllo che gli altri parametri siano dei percorsi
 percorsi=
-for i 
+for i
 do
     case $i in
-        */*) 
-            if test ! -d $i -o ! -x $i
-            then 
-                echo ERRORE: parametro $i non valido
-                echo SUGGERIMENTO: inserire un percorso assoluto di una directory
-                exit 2
-            fi
-            percorsi="$percorsi $i"
-            ;;
-        *) 
-            echo ERRORE: parametro $i non valido
-            echo SUGGERIMENTO: inserire un percorso di una directory
+    */*)
+        if test ! -d $i -o ! -x $i
+        then
+           echo ERRORE: il parametro $i non è valido
+           echo SUGGERIMENTO: inserire una directory traversabile in forma assoluta
             exit 3
-            ;;
+        fi
+        percorsi="$percorsi $i"
+        ;;
+    *)
+        echo ERRORE: il parametro $i non è valido
+        echo SUGGERIMENTO: inserire una directory traversabile in forma assoluta
+        exit 4
+        ;;
     esac
 done
 
+#Aggiorno la variabile ambiente PATH
 PATH=`pwd`:$PATH
 export PATH
 
-tmp1="/tmp/estensione1"
-tmp2="/tmp/estensione2"
+#Creao i file temporanei che conterranno le occorrenze
+tmp1="/tmp/occorrenzeS1"
 > $tmp1
+
+tmp2="/tmp/occorenzeS2"
 > $tmp2
 
 
+#Invocazione iterativa del file comandi ricorsivo
 for G in $percorsi
 do
     FCR.sh $G $S1 $S2 $tmp1 $tmp2
-done
-#Conto le righe del file dove ho salvato le occorrenze 
-NRO1=`wc -l < $tmp1`
-NRO2=`wc -l < $tmp2`
+done   
 
+TOT1=`wc -l  < $tmp1`
 
-if test $NRO1 -ge $NRO2
+echo SONO STATE TROVATE $TOT1 OCCORRENZE...
+if test ! $TOT1 -eq 0
 then
-    echo inserisci un numero tra 1 e $NRO2
-    read selected
-    echo `head -n $selected $tmp1 | tail -n 1 $tmp1`
+    echo ======================
+    cat $tmp1
+    echo ======================
 fi
 
-tot=`expr $NRO1 + $NRO2`
-echo Sonoo state trovate $tot occorrenze globali
-echo LISTA OCCORRENZE TROVATE S1: $NRO1
-echo ================================
-echo `cat $tmp1`
-echo ================================
-echo LISTA OCCORRENZE TROVATE S2: $NRO2
-echo ================================
-echo `cat $tmp2`
-echo ================================
 
-rm $tmp1
+
+TOT2=`wc -l  < $tmp2`
+
+echo SONO STATE TROVATE $TOT2 OCCORRENZE...
+if test ! $TOT2 -eq 0
+then
+    echo ======================
+    cat $tmp2
+    echo ======================
+fi
+
+
+if test $TOT1 -le $TOT2
+then
+    echo Inserire un unmero compreso tra 1 e $TOT1
+    read X
+    echo
+    if test $X -gt $TOT1 -o $X -lt 1
+    then
+        echo ERRORE: Valore inserito non valido
+        echo SUGGERIMENTO: Inserire un unmero compreso tra 1 e $TOT1
+        exit 5
+    fi
+    echo Riga $X del file contenente le occorrenze con estensione $S1...
+    echo `head -n $X $tmp1| tail -n 1 $tmp1`
+    echo
+    echo Riga $X del file contenente le occorrenze con estensione $S2...
+    echo `head -n $X $tmp2 | tail -n 1 $tmp2`
+fi
+
+
+
 rm $tmp2
-
-
-
+rm $tmp1    
--- Shell/Esame12Apr23/FCR.sh
+++ Shell/Esame12Apr23/FCR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #FIle comandi ricorsivo inerente alla prova d'esame del 12 Aprile 2023
-
+echo FCR
 #Mi sposto nella directory da analizzare
 cd $1
 
@@ -10,21 +10,24 @@ S2=$3
 tmp1=$4
 tmp2=$5
 
-
 #Cerco le occorrenze
+
 for i in *
-do
-    if test -f $i -a -x $i 
+do  
+    echo analizzo $i
+    if test -f $i -a -r $i
     then
         case $i in
-                *.$S1)
-                    echo $i ha estenzione S1
-                    echo "$(pwd)/$i" >> "$tmp1"
-                    ;;
-                *.$S2) 
-                    echo $i ha estenzione S2
-                    echo "$(pwd)/$i" >> "$tmp2"
-                    ;;  
+            *.$S1)
+                    echo il file $i ha estensione $S1
+                    echo `pwd`/$i >> $tmp1
+                    
+                ;;
+            *.$S2)
+                    echo il file $i ha estensione $S2
+                    echo `pwd`/$i >> $tmp2
+
+                ;;
         esac
     fi
 
@@ -32,9 +35,8 @@ done
 
 for i in *
 do
-        if test -d $i -a -x $i
-        then
-                #invocazione ricorsiva
-                FCR.sh `pwd`/$i $S1 $S2 $tmp1 $tmp2
-        fi
+   if test -d $i -a -x $i
+   then
+       $0 `pwd`/$i $S1 $S2 $tmp1 $tmp2
+   fi
 done
